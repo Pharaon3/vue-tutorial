8e26af2e7a3ffff9717f6be556c11a29b9b4cdf1
Pharaon3 <Pharaon313@outlook.com>
2023-03-27 08:21:49 +0900

    Rendering a list of Vue components

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
-      <li>
-        <ToDoItem label="My Todo Item" :done="false" />
+      <li v-for="item in ToDoItems" :key="item.id">
+        <ToDoItem 
+          :id="item.id"
+          :label="item.label" 
+          :done="item.done" />
       </li>
     </ul>
   </div>
@@ -11,10 +14,22 @@
 
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
+
 export default {
   name: 'App',
   components: {
     ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        {id: uniqueId("todo-"), label: "Learn Vue", done: false},
+        {id: uniqueId("todo-"), label: "Create a Vue project with the CLI", done: true },
+        {id: uniqueId("todo-"), label: "Have fun", done: true },
+        {id: uniqueId("todo-"), label: "Create a to-do list", done: false},
+      ]
+    }
   }
 }
 </script>
