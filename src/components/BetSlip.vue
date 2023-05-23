c0f4f96646c2ef9ce10c09a3840ffd0bac6e0e3a
Pharaon3 <Pharaon313@outlook.com>
2023-05-23 06:38:02 -0400

    Bet Slip

--- src/components/BetSlip.vue
+++ src/components/BetSlip.vue
@@ -11,7 +11,7 @@
       <div class="betInfoTitle">Singles</div>
       <div class="divider" />
       <div class="betcard">
-        <div class="flex">
+        <div class="flex" style="align-items: baseline;">
           <div style="cursor: pointer;"><i class="fa fa-times" aria-hidden="true"></i></div>
           <div style="text-align: left;">
             <div class="betcard-header">Man City</div>
@@ -19,6 +19,33 @@
             <div>Brighton vs Man City</div>
           </div>
         </div>
+        <div class="flex" style="align-items: baseline;">
+          <div style="font-weight: bold;">-125</div>
+          <div class="inputbox">
+            <i class="fa fa-usd" aria-hidden="true"></i>
+            <input type="number" placeholder="0.00">
+          </div>
+        </div>
+      </div>
+      <div class="betcard">
+        <div class="flex" style="align-items: baseline;">
+          <div style="cursor: pointer;"><i class="fa fa-times" aria-hidden="true"></i></div>
+          <div style="text-align: left;">
+            <div class="betcard-header">Man City</div>
+            <div>Moneyline</div>
+            <div>Brighton vs Man City</div>
+          </div>
+        </div>
+        <div class="flex" style="align-items: baseline;">
+          <div style="font-weight: bold;">-125</div>
+          <div class="inputbox">
+            <i class="fa fa-usd" aria-hidden="true"></i>
+            <input type="number" placeholder="0.00">
+          </div>
+        </div>
+      </div>
+      <div class="betButton">
+        Bet Now
       </div>
     </div>
   </div>
@@ -37,6 +64,49 @@ export default {
 };
 </script>
 <style>
+.betButton {
+  border-radius: 10px;
+  background-color: #6f6;
+  color: white;
+  padding: 10px 30px;
+  cursor: pointer;
+}
+.betButton:hover {
+  background-color: #0f0;
+  color: white;
+}
+.betButton:active {
+  background-color: #050;
+  color: #eee;
+}
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  appearance: none;
+  margin: 0;
+}
+input {
+  border-style: none;
+  width: 100%;
+  text-align: right;
+  font-size: 14px;
+}
+input:focus {
+  outline: none;
+}
+.inputbox {
+  height: 40px;
+  width: 90px;
+  border-radius: 7px;
+  border-style: solid;
+  border-width: 1px;
+  text-align: right;
+  padding-right: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 10px;
+}
 .betcard-header {
   font-size: 14px;
   font-weight: bold;
@@ -55,6 +125,7 @@ export default {
 .betslip-header-help {
   font-size: 14px;
   text-transform: initial;
+  cursor: pointer;
 }
 
 .betslip-header {
